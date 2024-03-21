2d48f44dbbdd72adbe6bbbc9b3c76d4d3a45fba0
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-03-21 09:34:14 +0000

    non parametric version

--- LSFR/lsfr.sv
+++ LSFR/lsfr.sv
@@ -1,10 +1,23 @@
 module lsfr (d_i, d_o, clk, rss);
-  input logic clk;
-  input logic rss;
-  input logic d_i;
-  output logic d_o;
+  
+  input   logic         clk;
+  input   logic         rss;
+  input   logic         d_i;
+  output  logic         d_o;
+
+  logic [15:0]  r;
+  logic [15:0]  r_n;
+  logic [14:0]  r_s;
+
+  always_comb
+  begin:
+    r_s = r >> 1; // take last 15 bits of r
+  end
 
   always_ff @( posedge clk )
   begin:
-    
+    r_n <= {d_i, r_s} // append d_i to r
+    d_o <= r_n[0] ^ r_n[2] ^ r_n[3] ^ r_n[5];
   end
+
+endmodule: lsfr
---
+++ LSFR/lsfr_tb.sv
@@ -0,0 +1,29 @@
+`timescale 1ns/10ps
+module tb_lsfr ();
+    parameter HALF_CLK = 5;
+
+    logic clk;
+    logic d_i;
+    logic d_o;
+    logic rss;
+
+    lsfr lsfr_i (
+        .d_i(d_i),
+        .d_o(d_o),
+        .rss(rss),
+        .clk(clk)
+    )
+
+    always #HALF_CLK clk = ~ clk;
+
+    initial
+    begin
+        clk <= '0;
+        rss <= '0;
+        d_o <= '0;
+        d_i <= '0;
+
+        
+    end
+
+
